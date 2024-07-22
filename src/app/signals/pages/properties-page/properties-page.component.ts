d79f3aba5930ce8d352d5362853cc40e3a602b4a
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2024-07-22 11:11:05 -0600

    Sección 24: Signals - Primeras señales y mutaciones

---
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './properties-page.component.html',
+  styleUrl: './properties-page.component.css'
+})
+export class PropertiesPageComponent {
+
+  onFieldUpdated(field: string, value: string){
+    console.log({field, value})
+  }
+
+}
--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SignalsRoutingModule } from './signals-routing.module';
 import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
+import { CounterPageComponent } from './pages/counter-page/counter-page.component';
+import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
-    SignalsLayoutComponent
+    SignalsLayoutComponent,
+    CounterPageComponent,
+    UserInfoPageComponent,
+    PropertiesPageComponent,
+    SideMenuComponent
   ],
   imports: [
     CommonModule,
