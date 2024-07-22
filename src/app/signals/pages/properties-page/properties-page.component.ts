de6205065e4c48a49d112da03b8fa02f8937099e
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2024-07-22 11:51:38 -0600

    Seccion 24: Signals

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -6,8 +7,47 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
-  onFieldUpdated(field: string, value: string){
-    console.log({field, value})
+  public user = signal<User>({
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg'
+  });
+
+  public fullName = computed( () => `${this.user().first_name} ${this.user().last_name}`);
+
+  onFieldUpdated(field: keyof User, value: string){
+
+    this.user.update( current => {
+      switch(field){
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'id':
+          current.id = Number(value);
+      }
+
+      return current;
+    });
+
+    /* this.user.set({
+      ...this.user(),
+      [field]: value,
+    }) */
   }
 
 }
